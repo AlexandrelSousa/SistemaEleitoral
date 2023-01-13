bdb64e1266d9a871841eb7995ce0a86b77c24d11
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-01-13 01:30:03 -0200

    criando o CRUD usuarios

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const userController = require('./controllers/userController')
 
-routes.get('/users', userController.index)
+routes.get('/users', userController.index);
+routes.post('/users', userController.create);
+routes.put('/users/:id', userController.update);
+routes.delete('/users/:id', userController.delete);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ const express = require ('express');
 const app = express();
 const routes = require('./routes');
 
+app.use(express.json());
 app.use(routes);
+
+//catch all
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({error: error.message})
+});
 
 app.listen(8080, ()=> console.log('servidor rodando!'));
