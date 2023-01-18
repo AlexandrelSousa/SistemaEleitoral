e91393b37d898730628f634473f7b8013e1f4575
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-01-18 17:51:28 -0200

    alteração de rotas da aplicação no arquivo app-routing.module.ts

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UsersAddComponent } from './users-add/users-add.component';
+import { UsersEditComponent } from './users-edit/users-edit.component';
+import { UsersGetComponent } from './users-get/users-get.component';
+
+const routes: Routes = [
+  { path: 'users/create', component: UsersAddComponent},
+  { path: 'users/:id', component: UsersEditComponent},
+  { path: 'users', component: UsersGetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
