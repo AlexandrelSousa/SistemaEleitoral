355fb4d5d299c6107d8f41c0c554daf8bf68ede6
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-02-04 20:08:39 -0200

    criando o modulo de login e configurando a rota

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './components/login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login',
+    loadChildren: () => import('./components/login/login.module').then(m => LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
